55b4d7c999d1475c805587c7e0e13f86514f79c6
moldcode <kabi.mold@gmail.com>
2015-01-13 21:36:56 +0900

    mod sagaworld/properties

--- SagaWorld/Properties/AssemblyInfo.cs
+++ SagaWorld/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SagaWorld")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SagaWorld")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
