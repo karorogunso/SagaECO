11bacfb9eba05df393318b6e9e0c1cbeb18fe109
moldcode <kabi.mold@gmail.com>
2015-01-13 13:40:22 +0900

    VS 2013

---
+++ SagaMap/Network/Client/MapClient.Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SagaDB;
+using SagaDB.Actor;
+using SagaLib;
+using SagaMap;
+using SagaMap.Manager;
+
+namespace SagaMap.Network.Client
+{
+    public partial class MapClient
+    {
+
+
+    }
+}
